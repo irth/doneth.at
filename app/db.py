7265866fe2198888611e81eadfef2cb77be4a32f
wojciech <me@irth.pl>
2020-08-29 17:41:18 +0000

    Implement accomplishment logging functionality
    Allow browsing historical accomplishments.
    
    Created a timeutils module, which contains all time-related logic, to
    allow easier extension later (for when I'll be implementing timezones
    and custom day-start hours)
    
    show today's accomplishements on the main page
    
    extract main.handle_accomplishment_submission function
    
    handle adding new accomplishments to the database
    
    add header and add accomplishment UI
    
    extract .card css component
    
    add Accomplishment model
    
    Co-authored-by: Wojciech Kwolek <wojciech@kwolek.xyz>
    Reviewed-on: https://git.r23s.eu/wojciech/doneth.at-backend/pulls/1

--- app/db.py
+++ app/db.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_migrate import Migrate
+from datetime import datetime, timedelta
+
+from . import timeutils
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -22,6 +25,9 @@ class User(UserMixin, db.Model):
     last_login = db.Column(db.DateTime, index=False,
                            unique=False, nullable=True)
 
+    accomplishments = db.relationship(
+        'Accomplishment', backref='user', lazy=True)
+
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
@@ -30,3 +36,37 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Accomplishment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    time = db.Column(db.DateTime(), nullable=False,
+                     default=db.func.current_timestamp())
+    text = db.Column(db.String(128), nullable=False)
+    difficulty = db.Column(db.Integer)
+
+    @property
+    def difficulty_class(self):
+        if self.difficulty <= 5:
+            return "easy"
+        if self.difficulty <= 10:
+            return "medium"
+        return "hard"
+
+    @staticmethod
+    def get_time_range(user_id, start, end):
+        return Accomplishment.query.filter(
+            Accomplishment.time >= start, Accomplishment.time < end, Accomplishment.user_id == user_id).all()
+
+    @staticmethod
+    def get_day(user_id, day):
+        # TODO: allow setting custom "start of day" hour
+        start = timeutils.day(day)
+        end = timeutils.day_after(day)
+        return Accomplishment.get_time_range(user_id, start, end)
+
+    @staticmethod
+    def get_today(user_id):
+        today = datetime.now()
+        return Accomplishment.get_day(user_id, today)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,78 @@
-from flask import Blueprint, render_template
+from . import timeutils
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+from .db import db, Accomplishment
+from datetime import datetime, timedelta
 
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
-def index():
-    return render_template('index.html')
+class NewAccomplishementForm(FlaskForm):
+    text = StringField('Accomplishment', validators=[
+                       DataRequired(), Length(max=128)])
+    submit_5 = SubmitField('5 XP')
+    submit_10 = SubmitField('10 XP')
+    submit_15 = SubmitField('15 XP')
+
+
+def handle_accomplishment_submission(form):
+    accomplishment = Accomplishment()
+    accomplishment.user_id = current_user.id
+    accomplishment.text = form.text.data
+    accomplishment.difficulty = 5
+    if form.submit_10.data:
+        accomplishment.difficulty = 10
+    elif form.submit_15.data:
+        accomplishment.difficulty = 15
+    # the timestamp should be set by the database
+    db.session.add(accomplishment)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+
+@main.route('/', defaults={'day': 'today'}, methods=['GET', 'POST'])
+@main.route('/day/<day>')
+def index(day):
+    day_datetime = None
+    day_string = None
+    is_today = False
+    if day == "today":
+        day_datetime = timeutils.today()
+        day_string = "Today"
+        is_today = True
+    else:
+        day_datetime = timeutils.from_str(day)
+        if timeutils.is_today(day_datetime):
+            return redirect('/')
+        day_string = timeutils.as_fancy_str(day_datetime)
+
+    if not current_user.is_authenticated:
+        return render_template('index.html')
+
+    form = NewAccomplishementForm()
+    if form.validate_on_submit():
+        return handle_accomplishment_submission(form)
+
+    accomplishments = list(reversed(Accomplishment.get_day(
+        current_user.id, day_datetime)))
+    total = sum(a.difficulty for a in accomplishments)
+
+    tomorrow = timeutils.day_after(day_datetime)
+    yesterday = timeutils.day_before(day_datetime)
+
+    if timeutils.is_future(tomorrow):
+        tomorrow = None
+
+    return render_template(
+        'main/app.html',
+        form=form,
+        day=day_string,
+        accomplishments=accomplishments,
+        total=total,
+        tomorrow=timeutils.as_str(tomorrow),
+        yesterday=timeutils.as_str(yesterday),
+        is_today=is_today,
+    )
