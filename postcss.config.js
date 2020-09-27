716055cf6a53d51d8b86b2cd73e0b459a3133447
Wojciech Kwolek <me@irth.pl>
2020-09-27 17:50:12 +0000

    CSS (forms) refactoring (#13)
    CSS (forms) refactoring
    
    * All forms now use the render_field helper.
    * CSS is now compiled using PostCSS.
    * Common form styles got extracted to more reusable & flexible classes.
    
    Closes #12.
    
    Co-authored-by: Wojciech Kwolek <wojciech@kwolek.xyz>
    Reviewed-on: https://git.r23s.eu/wojciech/doneth.at-backend/pulls/13

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+const plugins = [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+if (process.env.NODE_ENV == "production") {
+    plugins.push(require('cssnano')({
+        preset: 'default',
+    }))
+}
+
+module.exports = {
+    plugins
+}
